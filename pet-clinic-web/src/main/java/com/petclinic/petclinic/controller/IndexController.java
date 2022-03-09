1741f5f32dca55379859af3007419b75369d11f5
robbinishere <mailtorobbin@gmail.com>
2022-03-09 05:40:20 -0500

    Add layout to petclinic webapp  closes #9

--- pet-clinic-web/src/main/java/com/petclinic/petclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/petclinic/petclinic/controller/IndexController.java
@@ -1,4 +1,12 @@
 package com.petclinic.petclinic.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("vets")
+
 public class IndexController {
+    @RequestMapping("/")
+    public String welcomepage(){
+    return "index";
+}
 }
