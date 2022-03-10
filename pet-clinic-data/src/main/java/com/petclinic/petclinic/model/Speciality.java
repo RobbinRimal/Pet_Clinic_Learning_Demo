f186c0ad946f569162aa9672532c36941f00ef5e
robbinishere <mailtorobbin@gmail.com>
2022-03-10 03:45:40 -0500

    Add Speciality and update Vet closes #17

---
+++ pet-clinic-data/src/main/java/com/petclinic/petclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.petclinic.petclinic.model;
+
+public class Speciality extends BaseEntity{
+private String discription;
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
--- pet-clinic-data/src/main/java/com/petclinic/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/petclinic/petclinic/model/Vet.java
@@ -2,6 +2,17 @@ package com.petclinic.petclinic.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class Vet extends Person{
+    private Set<Speciality> speciality;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
