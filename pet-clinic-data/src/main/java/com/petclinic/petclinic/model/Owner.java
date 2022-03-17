db87a080ab9896f14d3920b1c9e5f9011f07f4b0
robbinishere <mailtorobbin@gmail.com>
2022-03-16 22:35:47 -0400

    Implement Base entity done Closes #21

--- pet-clinic-data/src/main/java/com/petclinic/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/petclinic/petclinic/model/Owner.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashSet;
 import java.util.Set;
 
-@Component
+
 public class Owner extends Person{
     private Set<pet> pets= new HashSet<>();
     private String address;
