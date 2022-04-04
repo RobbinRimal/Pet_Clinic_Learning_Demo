28bfce26456b365d1fad7e09b295945491bdafc7
robbinishere <mailtorobbin@gmail.com>
2022-04-04 05:21:27 -0400

    finally making some progress with test

---
+++ pet-clinic-data/src/test/java/com/petclinic/petclinic/service/springdatajpa/OwnerSDjpaServiceTest.java
@@ -0,0 +1,55 @@
+package com.petclinic.petclinic.service.springdatajpa;
+
+import com.petclinic.petclinic.reposotaries.OwnerReposataries;
+import com.petclinic.petclinic.reposotaries.PetReposataries;
+import com.petclinic.petclinic.reposotaries.PetTypeRepositaries;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerSDjpaServiceTest {
+    @Mock
+    OwnerReposataries ownerReposataries;
+    PetReposataries petReposataries;
+    PetTypeRepositaries petTypeRepositaries;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByLastname() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleet() {
+    }
+
+    @Test
+    void deleetById() {
+    }
+
+    @Test
+    void testFindByLastname() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+}
